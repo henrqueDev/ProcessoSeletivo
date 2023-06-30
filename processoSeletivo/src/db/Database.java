737636ff5f463a4f231d679d1515300ad4c90159
henrique15775 <lhdeveloper137@gmail.com>
2023-06-29 22:10:25 -0300

    Adicionar mais candidatos

--- processoSeletivo/src/db/Database.java
+++ processoSeletivo/src/db/Database.java
@@ -24,9 +24,40 @@ public final class Database {
                                 "Sion",
                                 Set.of(new Titulo[] { new TituloDoutor() }),
                                 Set.of(new Experiencia[] {
+                                        new ExperienciaMagisterioIF(1),
+                                        new ExperienciaGestaoAcademicaCoordenacao(1),
+                                        new ExperienciaGestaoAcademicaCargoDirecao(1) }),
+                                79),
+                        new Candidato(
+                                "2.222.222",
+                                "Jão",
+                                Set.of(new Titulo[] { new TituloDoutor() }),
+                                Set.of(new Experiencia[] {
                                         new ExperienciaMagisterioIF(2),
                                         new ExperienciaGestaoAcademicaCoordenacao(2),
                                         new ExperienciaGestaoAcademicaCargoDirecao(2) }),
+                                89),
+                        new Candidato(
+                                "3.333.333",
+                                "Chico Melancia",
+                                Set.of(new Titulo[] { new TituloDoutor() }),
+                                Set.of(new Experiencia[] {
+                                        new ExperienciaMagisterioIF(3),
+                                        new ExperienciaGestaoAcademicaCoordenacao(3) }),
+                                70),
+                        new Candidato(
+                                "4.444.444",
+                                "Carla",
+                                Set.of(new Titulo[] { new TituloDoutor() }),
+                                Set.of(new Experiencia[] {
+                                        new ExperienciaMagisterioIF(2) }),
+                                99),
+                        new Candidato(
+                                "5.555.555",
+                                "Fabiana",
+                                Set.of(new Titulo[] { new TituloDoutor() }),
+                                Set.of(new Experiencia[] {
+                                        new ExperienciaGestaoAcademicaCoordenacao(5) }),
                                 69))));
 
     }
