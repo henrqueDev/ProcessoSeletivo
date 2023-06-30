e36df72fab1dc8e9969eef86f051733f23ed3497
henrique15775 <lhdeveloper137@gmail.com>
2023-06-29 22:58:42 -0300

    Refatorar #1

--- processoSeletivo/src/edital/Edital.java
+++ processoSeletivo/src/edital/Edital.java
@@ -26,18 +26,31 @@ public final class Edital {
 
     public void processarAvaliacoes() {
         this.provaDesempenho.setNext(this.provaTitulo);
+        this.iniciarProvas();
+        this.checarCandidatosReprovados();
+        this.ordernarCandidatosClassificados();
+        this.exibirClassificacaoFinal();
+    }
+
+    private void iniciarProvas() {
         for (Candidato candidato : this.candidatos) {
             this.provaDesempenho.handle(candidato);
         }
+    }
 
+    private void checarCandidatosReprovados() {
         for (Candidato candidato : new ArrayList<Candidato>(this.candidatos)) {
             if (!candidato.isClassificado()) {
                 this.candidatos.remove(candidato);
             }
         }
+    }
 
-        candidatos.sort(Comparator.comparing(Candidato::getPontuacaoTotal).reversed());
+    private void ordernarCandidatosClassificados() {
+        this.candidatos.sort(Comparator.comparing(Candidato::getPontuacaoTotal).reversed());
+    }
 
+    private void exibirClassificacaoFinal() {
         for (Candidato candidato : candidatos) {
             System.out.println((candidatos.indexOf(candidato) + 1) + "° Lugar - " + candidato.getNome() + " - "
                     + candidato.getPontuacaoTotal() + " pontos");
